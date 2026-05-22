f55b529916d524a9affedc75f4c07a5d8e5c6e0f
navaneesreddy <navaneesreddy@gmail.com>
2026-05-22 21:17:24 +0530

    hello world script created

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hey $PERSON2 ,We are going to bomb you"
+echo "$PERSON2: We are going to bomb you too"
+echo "$PERSON1: Hey $PERSON2 I am serious"
+echo "$PERSON2: We are also serious"
