92e207d00a6cdcf7971b6e4a2323137352e206c9
navaneesreddy <navaneesreddy@gmail.com>
2026-05-23 22:10:56 +0530

    installation

--- 09-install-fun.sh
+++ 09-install-fun.sh
@@ -7,6 +7,6 @@ if [ $? -eq 0 ]; then
     echo "MySQL is already installed ... SKIPPING"
 else
     echo "Installing MySQL"
-    dnf install mysql -y
-    VALIDATE MySQL $?
+    dnf install mysql -y    
+    VALIDATE MySQL $? "Failed to install MySQL"
 fi
