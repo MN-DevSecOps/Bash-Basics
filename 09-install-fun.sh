0eb109a17cd3f3b912e16e68e0cb2dea610d38c0
navaneesreddy <navaneesreddy@gmail.com>
2026-05-23 22:17:25 +0530

    installation

--- 09-install-fun.sh
+++ 09-install-fun.sh
@@ -10,3 +10,12 @@ else
     dnf install mysql -y    
     VALIDATE MySQL $? "Failed to install MySQL"
 fi
+
+dnf list installed nginx
+if [ $? -eq 0 ]; then
+    echo "nginx is already installed ... SKIPPING"
+else
+    echo "Installing nginx"
+    dnf install nginx -y
+    VALIDATE nginx $? "Failed to install nginx"
+fi
