ddb8b9f15f49a4d24233e5261e5afea24e0d4685
navaneesreddy <navaneesreddy@gmail.com>
2026-05-23 20:59:11 +0530

    variable 05 script updated

--- 07-datatype.sh
+++ 07-datatype.sh
@@ -3,3 +3,10 @@ NUM1=10
 NUM2=Linux
 SUM=$((NUM1+NUM2))
 echo "The sum of $NUM1 and $NUM2 is: $SUM"
+
+#Arrary
+FRUITS=("Apple" "Banana" "Cherry")
+echo "First fruit: ${FRUITS[0]}"
+echo "All fruits: ${FRUITS[@]}" 
+echo "second fruit: ${FRUITS[1]}"
+echo "third fruit: ${FRUITS[2]}"
