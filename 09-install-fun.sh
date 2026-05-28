6ff4016757be142eb433804a3de0e90ac96cded9
navaneesreddy <navaneesreddy@gmail.com>
2026-05-28 16:56:39 +0530

    installation

--- 09-install-fun.sh
+++ 09-install-fun.sh
@@ -1,40 +1,7 @@
 #!/bin/bash
-
-# echo "I am continuing..."
-dnf list installed mysql
-
-if [ $? -eq 0 ]; then
-    echo "MySQL is already installed ... SKIPPING"
-else
-    echo "Installing MySQL"
-    dnf install mysql -y    
-    VALIDATE MySQL $? "Failed to install MySQL"
+USERID=$(id -u)
+# Check root access or not
+if [ $USERID -ne 0 ]; then
+echo "Please run this script with root access"
 fi
-
-dnf list installed nginx
-if [ $? -eq 0 ]; then
-    echo "nginx is already installed ... SKIPPING"
-else
-    echo "Installing nginx"
-    dnf install nginx -y
-    VALIDATE nginx $? "Failed to install nginx"
-fi
-# echo "I am continuing..."
-dnf list installed mysql
-
-if [ $? -eq 0 ]; then
-    echo "MySQL is already installed ... SKIPPING"
-else
-    echo "Installing MySQL"
-    dnf install mysql -y
-    VALIDATE MySQL $?
-fi
-
-dnf list installed nginx
-if [ $? -eq 0 ]; then
-    echo "nginx is already installed ... SKIPPING"
-else
-    echo "Installing nginx"
-    dnf install nginx -y
-    VALIDATE nginx $?
-fi
+echo "I am continuing..."
