a537f5bea066973bb61fa8cc67f54214a4e1b8f4
navaneesreddy <navaneesreddy@gmail.com>
2026-05-31 13:00:46 +0530

    assignment

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -19,7 +19,7 @@
  #   echo "Given number $NUMBER is less than 20"
 #fi
 
-# Check if user provided a number
+# Check if user provided a number and then check if it is even or odd
 if [ $# -eq 0 ]; then
     echo "Usage: $0 <number>"
     exit 1
@@ -30,4 +30,30 @@ if [ $((NUMBER % 2)) -eq 0 ]; then
     echo "Given number $NUMBER is even"
 else
     echo "Given number $NUMBER is odd"
+fi
+
+# check the number is prime or not
+# Prime numbers must be greater than 1
+if [ $NUMBER -le 1 ]; then
+    echo "$NUMBER is NOT a prime number."
+    exit 0
+fi
+
+# Flag to track primality
+is_prime=1
+
+# Loop from 2 to sqrt(NUMBER)
+for (( i=2; i*i<=NUMBER; i++ ))
+do
+    if [ $((NUMBER % i)) -eq 0 ]; then
+        is_prime=0
+        break
+    fi
+done
+
+# Print result
+if [ $is_prime -eq 1 ]; then
+    echo "$NUMBER is a PRIME number."
+else
+    echo "$NUMBER is NOT a prime number."
 fi
