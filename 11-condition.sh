a93a0a726b65d1fde8c350d3bcc4d7686acfa773
navaneesreddy <navaneesreddy@gmail.com>
2026-05-31 13:16:32 +0530

    installation

--- 11-condition.sh
+++ 11-condition.sh
@@ -2,7 +2,8 @@
 # Verify the today is weekend or not
 DAY=$(date +%u) # %u - day of week (1-7) 1 is Monday and 7 is Sunday
 if [ $DAY -eq 6 ] || [ $DAY -eq 7 ]; then
-    echo "Today is weekend"
+    echo "Today is weekend & Enkoy your day off"
 else
-    echo "Today is not weekend"
+if [ $DAY -eq 1 ]; then
+    echo "Start of the week, let's get to work"
 fi
