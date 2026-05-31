f8271d4e16db6dd25c6d5065afb0c18edc0d4d10
navaneesreddy <navaneesreddy@gmail.com>
2026-05-31 13:21:36 +0530

    installation

--- 11-condition.sh
+++ 11-condition.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 # Verify the today is weekend or not
-DAY=$(date +%u) # %u - day of week (1-7) 1 is Monday and 7 is Sunday
-if [ $DAY -eq 6 ] || [ $DAY -eq 7 ]; then
-    echo "Today is weekend & Enkoy your day off"
+if [ $DAY -eq 1 ]; then
+    echo "Today is Monday → Go to work"
+elif [ $DAY -ge 2 ] && [ $DAY -le 5 ]; then
+    echo "Today is a weekday (Tue–Fri) → Go to school meeting"
 else
-    echo "Today is weekday & Time to work"
-fi  
+    echo "Today is weekend → Enjoy your day off"
+fi
