d2b1ba21a18d08fe53f9e132e132cd2a47d963e3
navaneesreddy <navaneesreddy@gmail.com>
2026-05-31 10:33:11 +0530

    assignment

--- 01-hello-worrld.sh
+++ 01-hello-worrld.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # This is Hello world script
 echo "Hello world"
-echo "Today date is : $(date) and time is : $(date +%T)"
+echo "Today date is : $(date) and time is : $(date '+%d-%m-%Y %H:%M:%S')"
 echo "Running user is : $USER"
 echo "Current working directory is : $PWD"
