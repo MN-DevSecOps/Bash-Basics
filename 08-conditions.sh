55eae2db8b202346d3773b866e312bf038ccc751
navaneesreddy <navaneesreddy@gmail.com>
2026-05-31 12:44:47 +0530

    assignment

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -8,11 +8,25 @@ NUMBER=$1
 # ne - not equal
 # ge - greater than or equal
 # le - less than or equal
+# % - modulus operator
 
-if [ $NUMBER -gt 20 ]; then
-    echo "Given number $NUMBER is greater to 20"
-elif [ $NUMBER -eq 20 ]; then
-    echo "Given number $NUMBER is equal to 20"
+
+#if [ $NUMBER -gt 20 ]; then     
+#    echo "Given number $NUMBER is greaterthan to 20"        
+#elif [ $NUMBER -eq 20 ]; then
+ #   echo "Given number $NUMBER is equal to 20"
+#else
+ #   echo "Given number $NUMBER is less than 20"
+#fi
+
+if [$# -eq 0 ]; then
+    echo "Please provide a number as an argument"
+    exit 1
+fi
+
+
+if [ $((NUMBER % 2)) -eq 0 ]; then
+    echo "Given number $NUMBER is even"
 else
-    echo "Given number $NUMBER is less than 20"
+    echo "Given number $NUMBER is odd"
 fi
