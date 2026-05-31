55fe27c24f0d6313f4bee54c5089f7a7bd5ae663
navaneesreddy <navaneesreddy@gmail.com>
2026-05-31 13:18:22 +0530

    installation

--- 11-condition.sh
+++ 11-condition.sh
@@ -4,6 +4,5 @@ DAY=$(date +%u) # %u - day of week (1-7) 1 is Monday and 7 is Sunday
 if [ $DAY -eq 6 ] || [ $DAY -eq 7 ]; then
     echo "Today is weekend & Enkoy your day off"
 else
-if [ $DAY -eq 1 ]; then
-    echo "Start of the week, let's get to work"
-fi
+    echo "Today is weekday & Time to work"
+fi  
